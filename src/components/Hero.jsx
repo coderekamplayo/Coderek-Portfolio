c0960e59cc9b494d18caf2f7db65227ebdf4f73e
Christian Derek Amplayo <coderek.amplayo@gmail.com>
2025-07-27 23:30:51 +0800

    All GOODS

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+import vscDarkPlus from "react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus.js";
+
 
 const fadeInUp = {
   initial: { opacity: 0, y: 20 },
