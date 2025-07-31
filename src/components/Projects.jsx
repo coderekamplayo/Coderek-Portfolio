81edabd45a58a2cb9dd27fc92f75593229a4ea2d
Christian Derek Amplayo <coderek.amplayo@gmail.com>
2025-07-31 21:48:37 +0800

    update Befit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -68,7 +68,7 @@ export const Projects = () => {
           <motion.div
             className="project-image"
             style={{
-              backgroundImage: "url('/projects/social-media.png')",
+              backgroundImage: "url('/projects/Befit.jpg')",
             }}
             whileHover={{ scale: 1.05 }}
             transition={{ duration: 0.2 }}
